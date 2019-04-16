9e041f3000619e7cceda6000d08f6bf08bbd6485
SGeorges <SGeorges@github.com>
2019-04-16 09:12:58 -0400

    Base Structure of index HTML and Google Authentication

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
-// ============== Linking the database ==============
+var userID;
+
+// ============ Linking the Database ============
 
 var config = {
     apiKey: "AIzaSyDomvPd1Clur-_hGFZnN1Dos42Qk7op018",
@@ -16,33 +18,80 @@ var database = firebase.database();
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
-function googleSignin() {
-   firebase.auth()
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    'signInSuccessUrl': '/user.html',
+    'signInOptions': [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '',
+    // Privacy policy url.
+    privacyPolicyUrl: ''
+  };
+
+ui.start('#firebaseui-auth-container', uiConfig);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("this is happening");
+    
+    user.providerData.forEach(function (profile) {
+      
+      if (userID != profile.uid){
+        console.log(userID + " vs " + profile.uid);
+        userID = profile.uid;
+        database.ref(userID+"/login").push(true);  
+      }
+    });
+  } else {
+
+  }
+});
+
+// ============ Footer Button to display remaining description of site ============
+
+function moreInfo () {
+
+  // ----- Adding the fitness section to the main page -----
+  var fDiv = $("<div id= 'fitness-desc-row' class= 'row justify-content-start'>");
+    var fCol1 = $("<div id= 'desc-col' class= 'col-md-2'>");
+      $(fCol1).append("<img src='assets/images/main-fitness.jpg' width='100%' height='100%' class='d-inline-block' alt=''>");
+    var fCol2 = $("<div id= 'desc-col' class= 'col-md-6 my-auto'>");
+      $(fCol2).append("<p id= 'desc-text' class= 'h6 d-flex text-left'>Giving you the information available to perform a workout to your choosing</p>");
+
+    $(fDiv).append(fCol1);
+    $(fDiv).append(fCol2);    
+
+  // ----- Adding the fitness section to the main page -----
+  var mDiv = $("<div id= 'meals-desc-row' class= 'row justify-content-end'>");
+    var mCol1 = $("<div id= 'desc-col' class= 'col-md-6 my-auto'>");
+      $(mCol1).append("<p id= 'desc-text' class= 'h6 d-flex text-right'>Assisting in planning your meals based on calories <br> & ingredients</p>");
+    var mCol2 = $("<div id= 'desc-col' class= 'col-md-2'>");
+      $(mCol2).append("<img src='assets/images/main-food.jpg' width='100%' height='100%' class='d-inline-block' alt=''>");
+
+    $(mDiv).append(mCol1);
+    $(mDiv).append(mCol2);
+    
+  // ----- Adding the fitness section to the main page -----
+  var pDiv = $("<div id= 'personalize-desc-row' class= 'row justify-content-start'>");
+    var pCol1 = $("<div id= 'desc-col' class= 'col-md-2'>");
+      $(pCol1).append("<img src='assets/images/main-personalized.jpg' width='100%' height='100%' class='d-inline-block' alt=''>");
+    var pCol2 = $("<div id= 'desc-col' class= 'col-md-6 my-auto'>");
+      $(pCol2).append("<p id= 'desc-text' class= 'h6 d-flex text-left'>A free approach to fitness logging that allows you to make all the choices while we assist with providing the information you need!</p>");
+
+    $(pDiv).append(pCol1);
+    $(pDiv).append(pCol2);
    
-   .signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-		
-      console.log(token)
-      console.log(user)
-   }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+  // ----- Adding all 3 Divs to the Aspects container -----   
+  $("#aspects-container").append(fDiv);
+  $("#aspects-container").append(mDiv);
+  $("#aspects-container").append(pDiv);
 
-      var email = error.email;
-      var credential = error.credential;
-		
-      console.log(error.code)
-      console.log(error.message)
-   });
-}
+  // ----- Clearing the More Info Button from the HTML ----- 
+  $("#footer-container").empty();
 
-function googleSignout() {
-   firebase.auth().signOut()
-	
-   .then(function() {
-      console.log('Signout Succesfull')
-   }, function(error) {
-      console.log('Signout Failed')  
-   });
-}
+};
